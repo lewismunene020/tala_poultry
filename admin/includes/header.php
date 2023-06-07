43ecd480ccd531b2e6c6f600e8c98974b096a3ab
MuteeVincente <vinniemutee@gmail.com>
2023-06-07 14:13:13 +0300

    changing 'font-awsome' to font-awesome';

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -11,6 +11,6 @@ error_reporting(E_ALL);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>eCom Store Admin Panel</title>
     <link rel="stylesheet" href="css/bootstrap-337.min.css">
-    <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
--- includes/header.php
+++ includes/header.php
@@ -38,13 +38,13 @@
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <title>eCom Shop</title>
      <link rel="stylesheet" href="styles/bootstrap-337.min.css">
-     <link rel="stylesheet" href="font-awsome/css/font-awesome.min.css">
+     <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
      <link rel="stylesheet" href="styles/style.css">
  </head>
 
  <body>
 
-     <div id="top">
+     <div id="top"
          <!-- Top Begin -->
 
          <div class="container">
